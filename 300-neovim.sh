f10f228ceaac6fa53ec495766b73bba67abea886
MrStrik3 <MrStrik3@gmail.com>
2021-06-02 07:20:27 -0400

    Update neovim's installation

--- 300-neovim.sh
+++ 300-neovim.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/zsh
+#
+# Neovim's installation
 yay -S neovim-git
 git clone https://github.com/k-takata/minpac.git ~/.config/nvim/pack/minpac/opt/minpac
 stow nvim
+
+
+
+
+echo "=====> Don't forget to run ':PackUpdate' in neovim!!!"
