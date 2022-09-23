ecd8423fadd688a32532634eca8edc91321f9b2b
MrStrik3 <MrStrik3@gmail.com>
2022-09-23 07:58:49 -0400

    Add powershell config to toggleterm

--- nvim/.config/nvim/lua/config/lspconfig.lua
+++ nvim/.config/nvim/lua/config/lspconfig.lua
@@ -1,6 +1,16 @@
 local vim = vim
 local keymap = vim.keymap.set
 
+-- this is for diagnositcs signs on the line number column
+-- use this to beautify the plain E W signs to more fun ones
+-- !important nerdfonts needs to be setup for this to work in your terminal
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
 require('luasnip.loaders.from_vscode').lazy_load()
 
 -- LSP keymappings
@@ -109,16 +119,6 @@ require("null-ls").setup({
     require("null-ls").builtins.code_actions.gitsigns,
   },
 })
-
--- this is for diagnositcs signs on the line number column
--- use this to beautify the plain E W signs to more fun ones
--- !important nerdfonts needs to be setup for this to work in your terminal
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
 
 
 -- LSP SAGA  configs
--- nvim/.config/nvim/lua/config/toggleterm.lua
+++ nvim/.config/nvim/lua/config/toggleterm.lua
@@ -1,3 +1,18 @@
+if vim.fn.has('win32')  == 1 then
+  local powershell_options = {
+    shell = "pwsh",
+    shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+    shellquote = "",
+    shellxquote = "",
+  }
+
+  for option, value in pairs(powershell_options) do                                                                                                                                                                                                                                         vim.opt[option] = value
+    vim.opt[option] = value
+  end
+end
+
 require("toggleterm").setup({
     direction = 'float',
     shade_terminal = false,
