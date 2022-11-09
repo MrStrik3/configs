7cf99dd15429ba56290d6ab5c52ebeed80c41b8d
LefrancoisC <Carl.Lefrancois@dfo-mpo.gc.ca>
2022-11-09 11:39:40 -0500

    Update Powershell profile cfg

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,26 @@
-Set-Location C:\Users\LefrancoisC
+# Set-PSDebug -Trace 2;
 
-function scoop.reinstall {
-  Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+Set-Location C:\Users\LefrancoisC\Desktop\Travail
+
+$ENV:WK="C:\Users\LefrancoisC\Desktop\Travail"
+$ENV:TEMPDIR="C:\Users\LefrancoisC\Desktop\Temp"
+$ENV:JAVA_HOME="C:\Program Files\Java\jdk-11.0.13"
+$ENV:KUBE_EDITOR = "nvim"
+
+
+
+function cd.wk {
+  cd $ENV:WK
+}
+function cd.temp {
+  cd $ENV:TEMPDIR
+}
+
+function ps.import.docker {
+  Import-Module DockerCompletion
+}
+function ps.import.kb {
+  Import-Module PSKubectlCompletion
 }
 
 # RESET secret WHEN NEW SECRET ARE ADDED TO THE POD
@@ -14,28 +33,51 @@ function kb.sc.test {
   kubectl.exe config use-context akstest
 }
 function kb.sc.prod {
-  kubectl.exe config use-context aksprob-b
+  kubectl.exe config use-context aksprod-b
 }
 function kb.gc {
   kubectl.exe config get-contexts
 }
 function kb.gp {
-  # kubectl.exe -n iwls get pods -o wide
-  kubectl.exe -n dmp get pods -o wide
+  kubectl.exe -n iwls get pods -o wide
+  # kubectl.exe -n dmp get pods -o wide
 }
 function kb {
-  # kubectl.exe -n iwls $args
-  kubectl.exe -n dmp $args
+  kubectl.exe -n iwls $args
+  # kubectl.exe -n dmp $args
 }
 function kb.logs {
-  # kubectl.exe -n iwls logs $args
-  kubectl.exe -n dmp logs $args
+  kubectl.exe -n iwls logs $args
+  # kubectl.exe -n dmp logs $args
 }
 
+function neptune.ng.serve {
+  cd C:\Users\LefrancoisC\Desktop\Travail\CodeSource\Intellij\iwls\Neptune
+  ng serve
+}
+# function kb.dev.geo.console {
+#   kubectl.exe -n dmp exec -it geoserver-dev-geoserver-chart-0  -- bash
+# }
+#
+function mvn.dep.tree {
+    mvn dependency:tree
+  }
+
 function term.cfg.edit {
-   nvim %LOCALAPPDATA%\Microsoft\Windows Terminal\settings.json
+   nvim "%LOCALAPPDATA%\Microsoft\Windows Terminal\settings.json"
    # %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
 }
+
+
+# TERRAFORM cmds
+
+# function t.plan {
+#   terraform plan
+# }
+# function t.fmt {
+#   terraform fmt
+# }
+
 function vim {
   nvim $args
 }
@@ -50,44 +92,103 @@ function ll {
   dir $args
 }
 
-# AZ CLI commands 
+# AZ CLI commands
 function az.db.accesstoken {
-  az account get-access-token --resource-type oss-rdbms 
+  az account get-access-token --resource-type oss-rdbms
 }
 
-# tomcat 
+# tomcat
 function tomcat.start {
-  $env:JAVA_HOME = "C:\Program Files\Java\jre8"
-  $env:CATALINA_HOME = "C:\Users\LefrancoisC\Desktop\Travail\apps\apache-tomcat-8.5.71"
-  C:\Users\LefrancoisC\Desktop\Travail\apps\apache-tomcat-8.5.71\bin\catalina.bat start
+  $env:JAVA_HOME = "C:\Program Files\Java\jdk-11.0.13"
+  $env:CATALINA_HOME = "C:\Users\LefrancoisC\Desktop\Travail\apps\apache-tomcat-9.0.56"
+  $env:CATALINA_OPTS = "-DENTITY_RESOLUTION_ALLOWLIST=www.patate.com"
+  C:\Users\LefrancoisC\Desktop\Travail\apps\apache-tomcat-9.0.56\bin\catalina.bat start
 }
 
 function tomcat.stop {
-  C:\Users\LefrancoisC\Desktop\Travail\apps\apache-tomcat-8.5.71\bin\catalina.bat stop
+  $env:CATALINA_HOME = "C:\Users\LefrancoisC\Desktop\Travail\apps\apache-tomcat-9.0.56"
+  $env:JAVA_HOME = "C:\Program Files\Java\jdk-11.0.13"
+  C:\Users\LefrancoisC\Desktop\Travail\apps\apache-tomcat-9.0.56\bin\catalina.bat stop
+}
+
+function arch.start {
+  C:\Users\LefrancoisC\Desktop\Temp\wsl\archlinux\Arch.exe
 }
 
 function nvim.cfg {
-  nvim $env:USERPROFILE/AppData/Local/nvim/init.lua $env:USERPROFILE/AppData/Local/nvim/lua/plugins.lua $env:USERPROFILE/AppData/Local/nvim/lua/settings.lua $env:USERPROFILE/AppData/Local/nvim/lua/keymappings.lua
+  nvim $env:USERPROFILE/AppData/Local/nvim
 }
 
 function np {
   C:\Program Files (x86)\Notepad++\notepad++.exe $args
 }
 
-Import-Module PSKubectlCompletion
+
+#function geo.b {
+#  docker build . -t geoserver:2201
+#}
+#function geo.r {
+#  docker run -it -p 8081:8080 -e GEOSERVER_KEYCLOAK_SECRET="9a6ab865-07de-42b1-a102-430e90d7ec88" geoserver:2201
+#}
+
+function wsl.arch {
+  C:\Users\LefrancoisC\Desktop\Temp\wsl\archlinux\Arch.exe
+}
+
+
+# https://github.com/sakai135/wsl-vpnkit -- Fix WSL-VPN issues
+function wsl.vpnkit.start {
+  wsl -d wsl-vpnkit service wsl-vpnkit start
+}
+function wsl.vpnkit.stop {
+  wsl -d wsl-vpnkit service wsl-vpnkit stop
+}
+function wsl.vpnkit.update {
+  wsl --unregister wsl-vpnkit
+  wsl --import wsl-vpnkit $env:USERPROFILE\wsl-vpnkit wsl-vpnkit.tar.gz --version 2
+}
+
+function wsl.vpnkit.logs {
+  wsl.exe -d wsl-vpnkit tail -f /var/log/wsl-vpnkit.log
+}
+function wsl.vpnkit.cfg {
+  nvim /mnt/c/Users/LefrancoisC/wsl-vpnkit/wsl-vpnkit.conf
+}
+
+function wsl.vpnkit.defaults {
+  wsl -d wsl-vpnkit cat /app/defaults.conf
+}
+
+# PGSQL
+function docker.pgsql.start {
+  docker run -dp 5432:5432 -e POSTGRES_PASSWORD=support -e POSTGRES_DB="api-store" pgapiregistry:latest
+}
+function docker.wet.start {
+  docker run -dp 8080:8080 wettemplate:latest
+}
+
+function docker.azurite.start {
+  docker run -dp 10001:10001 -p 10000:10000 -p 10002:10002 mcr.microsoft.com/azure-storage/azurite
+}
+
+# Import-Module PSKubectlCompletion
 Set-Alias k -Value kubectl
 
-Register-KubectlCompletion
+# Register-KubectlCompletion
 # Add-Content --Path $PROFILE --Value "Import-Module PSKubectlCompletion"
 
-$ENV:MyPAT = "5qpq43f5ea4nnmemn3eroc7nuogcxkmgkukkbwmc47zpkccztrpa"
+$ENV:MyPAT = "huo3tuanqhseq2fmosjdrxzidp6s3cf7snezr6q4rd54xohxorga"
 
-Clear-Host
+#
+# Clear-Host
 
 # Welcome message
 # "You are now entering PowerShell : " + $env:Username
 $ENV:STARSHIP_CONFIG = "$HOME\.starship\config.toml"
 Invoke-Expression (&starship init powershell)
 
- 
- 
+
+
+
+
+Import-Module posh-git
