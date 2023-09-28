8dd316988d008eee3bdff8b59941e5a8a4244510
MrStrik3 <MrStrik3gmail.com>
2023-09-28 08:12:31 -0400

    Updated LSP works properly + update identline config to v3

--- nvim/.config/nvim/lua/config/plugins/indentlines.lua
+++ nvim/.config/nvim/lua/config/plugins/indentlines.lua
@@ -1,22 +1,26 @@
 local M = {
-	"lukas-reineke/indent-blankline.nvim",
-	event = "BufReadPre",
+  "lukas-reineke/indent-blankline.nvim",
+  event = "BufReadPre",
+  main = "ibl",
+  opts = {
+    -- use_treesitter = true,
+    -- space_char_blankline = " ",
+    -- show_end_of_line = true,
+    -- char = "┊", -- "⦙",
+    -- show_current_context = true,
+    -- show_current_context_start = true,
+    -- show_current_blankline_indent = false,
+    -- filetype_exclude = { "help", "packer", "terminal", "NvimTree" },
+    -- buftype_exclude = { "terminal", "nofile" },
+    -- char_highlight = "LineNr",
+    -- show_trailing_blankline_indent = false,
+    indent = { highlight = { "CursorColumn", "Whitespace" }, char = "" },
+    whitespace = {
+      highlight = { "CursorColumn", "Whitespace" },
+      remove_blankline_trail = false,
+    },
+    scope = { enabled = false },
+  }
 }
-
-function M.config()
-	require("indent_blankline").setup({
-		use_treesitter = true,
-		space_char_blankline = " ",
-		show_end_of_line = true,
-		char = "┊", -- "⦙",
-		show_current_context = true,
-		show_current_context_start = true,
-		show_current_blankline_indent = false,
-		filetype_exclude = { "help", "packer", "terminal", "NvimTree" },
-		buftype_exclude = { "terminal", "nofile" },
-		char_highlight = "LineNr",
-		show_trailing_blankline_indent = false,
-	})
-end
 
 return M
