832474ad572719a412db248a41de9860e5177e78
Lefrançois <Carl.Lefrancois@dfo-mpo.gc.ca>
2023-10-02 21:26:24 -0400

    Update AutohotKey +wezterm

--- autohotkey/main.ahk
+++ autohotkey/main.ahk
@@ -14,8 +14,12 @@ ShowOrRunApp(AppExe, WindowCondition) {
   }
   else {
     RunWait AppExe
+    if WinWait(WindowCondition, , 15)
+      WinActivate
   }
 }
+
+^!r::Reload
 
 #1::ShowOrRunApp("firefox.exe", "ahk_exe firefox.exe")
 <+#1::ShowOrRunApp("msedge.exe", "ahk_exe msedge.exe")
@@ -28,8 +32,11 @@ ShowOrRunApp(AppExe, WindowCondition) {
 #h::ShowOrRunApp(Format('explorer.exe "{1}\{2}"', UserDir, "Desktop\OneDrive - DFO-MPO"), "OneDrive - DFO-MPO ahk_exe explorer.exe ahk_class CabinetWClass")
 
 ; #Enter::ShowOrRunApp('WindowsTerminal.exe')
-#Enter::ShowOrRunApp("wt.exe --maximized", "ahk_exe WindowsTerminal.exe")
-#F2::ListWindows
+; #Enter::ShowOrRunApp("wt.exe --maximized", "ahk_exe WindowsTerminal.exe")
+#Enter::ShowOrRunApp(Format("wezterm-gui.exe --config-file {1}\Desktop\Temp\Repositories\configs\wezterm\wezterm_arch.lua", UserDir), "wslhost.exe ahk_exe wezterm-gui.exe")
+; <+#Enter::ShowOrRunApp(Format("wezterm-gui.exe --config-file {1}\Desktop\Temp\Repositories\configs\wezterm\wezterm_win.lua", UserDir), "pwsh.exe ahk_exe wezterm-gui.exe")
+<+#Enter::ShowOrRunApp("alacritty.exe", "alacritty")
+#F2::ListWindows()
 ; #F4::Run Format('explorer.exe /n /e ,"{1}"', "C:\Users\LefrancoisC\Desktop\Travail")
 
 ; Harpoon windows
---
+++ wezterm/wezterm_win.lua
@@ -0,0 +1,53 @@
+-- Pull in the wezterm API
+local wezterm = require('wezterm')
+local mux = wezterm.mux
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.window_close_confirmation = "NeverPrompt"
+config.window_decorations = "TITLE"
+
+config.default_prog = { 'pwsh.exe' }
+-- config.default_domain = 'WSL:Arch'
+
+-- Setting Theme & fonts
+config.color_scheme = 'Tokyo Night Storm'
+config.bold_brightens_ansi_colors = 'BrightAndBold'
+config.font = wezterm.font {
+  family = 'CaskaydiaCove Nerd Font Mono',
+  weight = 'Regular',
+  harfbuzz_features = {
+    'calt=1', 'ss01=1', -- Enable the cursive  form of the italic
+    'cv02=1',           -- Active special ligature for not equal
+    'cv30=1',           -- Longer pipe bar
+    'onum=1',
+    'ss02=1',  -- Change the default ligature for <= and >=
+    'ss04=1',  -- Change the dollar sign
+    'ss05=1',  -- Change the at sign
+    'ss08=1',  -- Change default ligature for ==, ===, != and !==
+    'ss19=1',  -- Enable a slashed zero
+    'zero=1'   -- Select a specific form of zero
+  }
+}
+
+config.font_size = 10.0
+
+wezterm.on("gui-startup", function()
+  local tab, pane, window = mux.spawn_window {}
+  window:gui_window():maximize()
+  -- window:gui_window():set_title('patate')
+end)
+
+config.enable_tab_bar = false
+
+config.scrollback_lines = 6000
+config.enable_scroll_bar = true
+
+return config
