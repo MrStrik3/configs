a55c6b57232beca8ca1928beb529b5972b0ff37f
MrStrik3 <MrStrik3gmail.com>
2025-03-21 21:24:34 -0400

    Add yay.caches.drop

--- shellrc/.aliases.sh
+++ shellrc/.aliases.sh
@@ -75,3 +75,13 @@ az.git.clone() {
 alias t.init="terragrunt init"
 alias t.plan="terragrunt plan"
 alias t.apply="terragrunt apply"
+
+
+alias yay.caches.drop='sudo paccache -rk3; yay -Sc --aur --noconfirm'
+alias yay.servers.update='export TMPFILE="$(mktemp)"; \
+    sudo true; \
+    rate-mirrors --save=$TMPFILE arch --max-delay=21600 \
+      && sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup \
+      && sudo mv $TMPFILE /etc/pacman.d/mirrorlist \
+      && ua-drop-caches \
+      && yay -Syyu --noconfirm'
