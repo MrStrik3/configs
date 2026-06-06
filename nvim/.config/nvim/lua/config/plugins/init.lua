6105544d9a524c91f43bf1aba1ad02c30aeff471
MrStrik3 <MrStrik3@gmail.com>
2026-06-06 19:36:50 -0400

    Add Snacks.nvim to nvim

--- nvim/.config/nvim/lua/config/plugins/init.lua
+++ nvim/.config/nvim/lua/config/plugins/init.lua
@@ -12,6 +12,32 @@ return {
   -- Requires nightly built neovim
   -- { "lewis6991/satellite.nvim", event = 'BufReadPost', opts = {} },
 
+  -- Bunch of useful features
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      bigfile = { enabled = true },
+      dashboard = { enabled = true },
+      explorer = { enabled = true },
+      indent = { enabled = true },
+      input = { enabled = true },
+      picker = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      terminal = { enabled = true },
+      -- words = { enabled = true },
+    },
+  },
+
   {
     "github/copilot.vim",
     lazy = false
